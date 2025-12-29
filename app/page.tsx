90c27d644d85848f2b421cefaf41256aa72d2742
Kleavant James <kleavant.olmedo@flexisourceitph.com>
2025-12-30 01:16:01 +0800

    connect supabase

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+'use client'
+
+import { useAuth } from "@/hooks/use-auth";
 import { redirect, RedirectType } from "next/navigation";
 
 export default function Home() {
+  const { user, isLoading } = useAuth()
+
+  if (user && !isLoading) {
+    redirect("/dashboard", RedirectType.replace);
+  }
 
   redirect("/login", RedirectType.replace);
-
-  // soon add a landing page or welcome screen here
 }
