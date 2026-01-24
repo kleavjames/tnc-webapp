99cc70c21480493a373cfc8109d66a94dd60f8d8
Kleavant James <kleavant.olmedo@flexisourceitph.com>
2026-01-24 14:12:28 +0800

    added glc schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,9 +1,11 @@
 import { defineSchema } from "convex/server";
 import { usersTable } from "./schemas/users";
 import { accountsTable, sessionsTable } from "./schemas/accounts";
+import { glcTable } from "./schemas/glc";
 
 export default defineSchema({
   users: usersTable,
   accounts: accountsTable,
   sessions: sessionsTable,
+  glc: glcTable,
 });
---
+++ convex/schemas/glc.ts
@@ -0,0 +1,12 @@
+import { defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export const glcTable = defineTable({
+  level: v.number(),
+  book: v.number(),
+  bookTitle: v.string(),
+  session: v.number(),
+  sessionTitle: v.string(),
+  createdAt: v.number(),
+  updatedAt: v.number(),
+})
